621d42ecd9c5ed473616eae3e55daa609ee3c336
oKiirus <77072551+oKiirus@users.noreply.github.com>
2021-06-02 10:54:25 -0700

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,47 @@
+import csv
+import pandas as pd
+import plotly.express as px
+import math 
+
+mean = 0
+
+with open('class1.csv', newline = '') as f:
+    file = csv.reader(f)
+    data = list(file)
+    
+data.pop(0)
+marks = []
+for i in range(0, len(data)):
+    q = data[i][1]
+    
+    marks.append(float(q))
+
+n = len(marks)
+sum = 0
+for e in marks:
+    sum += e
+
+mean = sum / n
+
+print(mean)
+
+
+
+df = pd.read_csv('class1.csv')
+
+graph = px.scatter(df, x = 'Student Number', y = 'Marks')
+graph.update_layout(shapes = [dict(type = 'line', y0 = mean, y1 = mean, x0 = 0, x1 = n)])
+graph.show()
+squaredList = []
+for a in marks:
+    b = int(a) - mean
+    b = b ** 2
+    squaredList.append(b)
+sigma = 0
+for i in squaredList:
+    sigma = sigma + i
+
+result = math.sqrt(sigma / (n - 1))
+
+print(result)
+
